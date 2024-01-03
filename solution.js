ba1e1bdfecbf852d2d22ea6eb45ff3700c4aaba3
Laura Wu <leemaiwu@gmail.com>
2024-01-02 17:49:26 -0700

    sovled

---
+++ solution.js
@@ -0,0 +1,12 @@
+const getCount = (str) => {
+  let count = 0
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+      count++
+    }
+  }
+  return count
+}
+
+console.log(getCount('laura'))
+console.log(getCount('chai'))
